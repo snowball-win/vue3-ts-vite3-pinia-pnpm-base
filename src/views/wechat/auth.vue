180fa45c884f48c69d08869da668bb0b69751bd9
李俊杰 <1091457391@qq.com>
2022-11-21 15:19:26 +0800

    feat: wx.config

--- src/views/wechat/auth.vue
+++ src/views/wechat/auth.vue
@@ -6,6 +6,29 @@
   </div>
 </template>
 <script setup lang="ts">
+import wx from 'weixin-js-sdk-ts'
+console.log('10wx', wx)
+
+// 这里通过调用接口获取参数
+// wx.config({
+//   debug: true, // 开启调试模式,调用的所有 api 的返回值会在客户端 alert 出来，若要查看传入的参数，可以在 pc 端打开，参数信息会通过 log 打出，仅在 pc 端时才会打印。
+//   appId: 'wxcc1124d410d52e8b', // 必填，公众号的唯一标识
+//   timestamp: 123, // 必填，生成签名的时间戳
+//   nonceStr: '', // 必填，生成签名的随机串
+//   signature: '', // 必填，签名
+//   jsApiList: ['previewImage', 'hideAllNonBaseMenuItem'] // 必填，需要使用的 JS 接口列表
+// });
+
+wx.ready(function(){
+  console.log('21wx', wx)
+  // config信息验证后会执行 ready 方法，所有接口调用都必须在 config 接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在 ready 函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在 ready 函数中。
+});
+wx.error(function(res){
+  console.log('25res', res)
+  // config信息验证失败会执行 error 函数，如签名过期导致验证失败，具体错误信息可以打开 config 的debug模式查看，也可以在返回的 res 参数中查看，对于 SPA 可以在这里更新签名。
+});
+
+
 // 微信网页授权
 // 判断是否是微信环境
 const isWechat = ()=> {
@@ -48,7 +71,9 @@ const getWxCode = (redirect_path: String) =>{
 	}
 }
 // 调用
-getWxCode('auth')
+getWxCode('#/login')
+
+console.log()
 
 </script>
 <style scoped lang="less">
