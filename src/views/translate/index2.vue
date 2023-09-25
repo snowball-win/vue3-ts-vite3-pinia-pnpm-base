09c1fc626f2e64835628bd1d45bc6d04038eee34
junjli@gkht.com <junjli@gkht.com>
2023-09-25 13:36:09 +0800

    feat: translate

--- src/views/translate/index2.vue
+++ src/views/translate/index2.vue
@@ -6,29 +6,46 @@
 </template>
 
 <script setup lang="ts" name="keepalive">
+// 加载script
+const loadScript = () => {
+  let script = document.createElement('script')
+  script.setAttribute("language", "javascript")
+  script.setAttribute("type", "text/javascript")
+  script.setAttribute("src", 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit')
+  document.getElementsByTagName('head')[0].appendChild(script)
+  script.onload = script.onreadystatechange = ((res:any)=>{
+    check()
+  })
+}
+loadScript()
+
+// 检测是否获取到google
+const check = () => {
+  console.log("10", google.translate.TranslateElement)
+  if(!google.translate.TranslateElement){
+    setTimeout(()=>{
+      check()
+    }, 200)
+  } else {
+    translate()
+  }
+}
+
+// 翻译
 const translate = () => {
   new google.translate.TranslateElement(
     {
         // pageLanguage: 'zh-CN',
-        // 需要翻译的语言，比如你只需要翻译成越南和英语，这里就只写en,vi
+        // 需要翻译的语言，比如你只需要翻译成英语，这里就只写en
         includedLanguages: 'en,zh-CN,hr,cs,da,nl,fr,de,el,iw,hu,ga,it,ja,ko,pt,ro,ru,sr,es',
         layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
         // 自动显示翻译横幅，就是翻译后顶部出现的那个
         autoDisplay: true,
-        // 还有些其他参数，由于原插件不再维护，找不到详细api了，将就了，实在不行直接上dom操作
+        // 还有些其他参数
     },
     'google_translate_element' // 触发元素的id
-  );
+  )
 }
-
-let script = document.createElement('script')    
-script.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'
-document.body.appendChild(script)
-script.onload = (()=>{
-  setTimeout(()=>{
-    translate()
-  },1000)
-})
 </script>
 
 <style scoped lang="less">
